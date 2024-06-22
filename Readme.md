902fd7e6d817ce1c970f78273fbd7c3bbdfadd19
JensenzhaoUT <Jensen.zhao@mail.utoronto.ca>
2024-06-21 21:50:35 -0400

    Add RDS DB information

--- Readme.md
+++ Readme.md
@@ -2,3 +2,4 @@
 rsync -avz --exclude-from='rsync-exclude.txt' \-e "ssh -i ~/.ssh/web-server-jensen.pem" \. ubuntu@ec2-204-236-200-61.compute-1.amazonaws.com:~/app
 
 # Contains DB information
+mysql -h database-anomaly-detection.clskoem8w7z5.us-east-1.rds.amazonaws.com -u admin -p database-anomaly-detection
--- src/models/db.ts
+++ src/models/db.ts
@@ -1,10 +1,10 @@
 import { createPool } from 'mysql2/promise';
 
 const pool = createPool({
-  host: 'your-db-host',
-  user: 'your-db-username',
-  password: 'your-db-password',
-  database: 'your-db-name',
+  host: 'database-anomaly-detection.clskoem8w7z5.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'jensen123',
+  database: 'database_anomaly_detection',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
