fea3a31b2d91c4f57b61bcbf7a404905860fd120
JensenzhaoUT <Jensen.zhao@mail.utoronto.ca>
2024-06-25 20:57:55 -0400

    HTML Report for E2E Test

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e-tests',
   timeout: 30000,
-  reporter: [['list'], ['json', { outputFile: 'playwright-report/results.json' }]],
+  reporter: [
+    ['list'],
+    ['json', { outputFile: 'playwright-report/results.json' }],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+  ],
   use: {
     headless: true,
     baseURL: 'http://44.219.129.210:3000',
