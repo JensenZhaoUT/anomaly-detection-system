647f19016b1c17ad6201c25d191ef3da685b591b
JensenzhaoUT <Jensen.zhao@mail.utoronto.ca>
2024-06-25 20:48:39 -0400

    Update Output Directory for e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e-tests',
-  timeout: 20000,
+  timeout: 30000,
+  reporter: [['list'], ['json', { outputFile: 'playwright-report/results.json' }]],
   use: {
-    headless: true, // Set to false if you want to see the browser actions
+    headless: true,
     baseURL: 'http://44.219.129.210:3000',
-    browserName: 'chromium', // You can also use 'firefox' or 'webkit'
+    browserName: 'chromium',
   },
 });
